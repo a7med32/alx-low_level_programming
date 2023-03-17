68cba3a0a232f444dae7235b01b2dd2b62d0a4b4
root <ahmed01281g@gmail.com>
2023-03-17 11:11:04 -0700

    print numbers-2

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main-Entry point
+ *
+ * Descriptin:A C program that prints with put function
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int x;
+
+	for (x = 0; ch <= 9; x++)
+	{
+		putchar (x);
+
+	}
+	putchar('\n');
+
+	return (0);
+}
