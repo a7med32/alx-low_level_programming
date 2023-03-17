d98f3e417d3b5a396036c9ef332d9467da36df2f
root <ahmed01281g@gmail.com>
2023-03-17 08:40:51 -0700

    betty fix

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -17,10 +17,7 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	/* your code goes there*/
-	
-        x = n % 10;
-
+	x = n % 10;
 	if (x == 0)
 	{
 		printf("Last digit of %d is 0 and is 0\n", n);
