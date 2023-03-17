3cf367bdcbdbded310a3c8672843d85e671484f4
root <ahmed01281g@gmail.com>
2023-03-17 11:07:21 -0700

    print numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main-Entry point
+ *
+ * Descriptin:A C program that prints with put function
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	printf("0123456789");
+
+	return (0);
+}
