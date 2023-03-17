4bfa49059cf1527cbbcb9b5763af5681bb117d45
root <ahmed01281g@gmail.com>
2023-03-17 09:28:28 -0700

    print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main-Entry point
+ *
+ * Descriptin:A C program that prints with put function
+ *
+ * Return: Always 0 (Success)
+ */
+
+
+int main(void)
+{
+
+	char ch;
+
+	char = 'a';
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+
+		putchar (ch);
+
+
+	return (0);
+}
+
