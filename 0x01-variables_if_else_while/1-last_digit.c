561d8692bad16c44190e402a96fca52ace82dc2b
root <ahmed01281g@gmail.com>
2023-03-17 08:32:45 -0700

    1-last_digit.c:20:2: error: ISO C90 forbids mixed declarations and code [-Werror=declaration-after-statement

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -13,18 +13,21 @@
 int main(void)
 {
 	int n;
+	int x;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there*/
-	int x = n % 10;
-	if (x ==  0)
+	
+        x = n % 10;
+
+	if (x == 0)
 	{
 		printf("Last digit of %d is 0 and is 0\n", n);
 	}
 	else if (x < 6 && x != 0)
 	{
-		printf("Last digit of %d is %d  and is less than 6 and not 0\n", n , x);
+		printf("Last digit of %d is %d  and is less than 6 and not 0\n", n, x);
 	}
 	else
 	{
