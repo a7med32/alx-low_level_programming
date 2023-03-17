066f02d2f72d33fd2bad92ae2f0f6d928ab75fd9
root <ahmed01281g@gmail.com>
2023-03-17 10:53:09 -0700

    exept q and e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main-Entry point
+ *
+ * Descriptin:A C program that prints with put function
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++){
+		if (ch != 'q' || ch != 'e')
+			putchar(ch)
+
+	}
+	putchar('\n');
+
+	return (0);
+}
