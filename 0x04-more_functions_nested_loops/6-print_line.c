793ec0989effa422b30dbd4c409e8c76dc2db59a
root <ahmed01281g@gmail.com>
2023-03-23 05:22:37 -0700

    commit

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,16 +1,17 @@
 #include "main.h"
 
 /**
- * more_numbers - print more numbers
+ * print_line - print more numbers
+ *@n:parameter
  */
 
 void print_line(int n)
 {	int i;
 
-	if (n > 0) 
+	if (n > 0)
 	{
 		for (i = 1; i <= n; i++)
 			_putchar('_');
 	}
 	_putchar('\n');
-}	
+}
