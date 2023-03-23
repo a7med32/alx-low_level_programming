41ff5fe96fd9ff8a42ae8710d325b9cf0215bdba
root <ahmed01281g@gmail.com>
2023-03-23 04:33:02 -0700

    digit *

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *mul- check if c is uppercase
+ *
+ *Description: 'the program's description'
+ *
+ * @c :the char
+ *
+ *Return: Always 0 (Success)
+ **/
+
+int mul(int a, int b)
+{
+
+	return (a * b);
+}
